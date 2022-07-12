966b4727fbc8efba4a5fe7c98e3068f2c6283611
Adityo Ij <kenzi.code@gmail.com>
2022-07-12 20:43:18 +0700

    first commit

---
+++ header.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package AgroChem
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'kr' ); ?></a>
+
+	<header id="masthead" class="relative">
+		<div class="topheader bg-slate-200 text-gray-800 text-sm">
+			<div class="max-w-7xl mx-auto py-2">
+				<div class="flex justify-between items-center">
+					<p>email@corporate.com</p>
+					<p>telp +62 021 2299 212</p>
+				</div>
+			</div>
+		</div>
+
+		<div class="mainheader bg-white">
+			<div class="max-w-7xl mx-auto py-6 items-center">
+
+				<div class="flex justify-between items-center">
+					<div class="w-[225px]">
+						<?php
+						the_custom_logo();
+						if ( is_front_page() && is_home() ) :
+							?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+							<?php
+						else :
+							?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+							<?php
+						endif;
+						$kr_description = get_bloginfo( 'description', 'display' );
+						if ( $kr_description || is_customize_preview() ) :
+							?>
+							<p class="site-description"><?php echo $kr_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+						<?php endif; ?>
+					</div><!-- .site-branding -->
+
+					<nav id="site-navigation" class="main-navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'kr' ); ?></button>
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location' => 'menu-1',
+								'menu_id'        => 'primary-menu',
+								'menu_class'	 => 'text-sm'
+							)
+						);
+						?>
+					</nav><!-- #site-navigation -->
+
+					<button class="hidden bg-[#F59E0B] py-3 px-6 text-white text-sm rounded-md md:block">
+              			Ask Quotation
+            		</button>
+				</div>
+			</div>
+
+
+		</div>
+		
+	</header><!-- #masthead -->
---
+++ homepage.php
@@ -0,0 +1,127 @@
+<?php
+/* 
+    Template Name: Homepage
+*/
+
+get_header();
+
+?>
+
+
+<main id="content" class="relative">
+
+    <!-- Hero -->
+    <div class="relative">
+        <div class="bg-[#FCFCFA]">
+            <div class="max-w-7xl mx-auto">
+                <div class="flex items-center justify-between py-20">
+                    <div class="hero space-y-5 py-20 w-2/5 mr-32">
+                        <h1 class="text-7xl font-bold text-[#B96D3C]">Synergising Agro-Outputs Eco-System</h1>
+                        <h5 class="text-2xl font-light">Uplifting Farmers & Connecting to Commerce</h5>
+                    </div>
+                    
+                    <div class="grid grid-cols-2 gap-4">
+                        <div class="bg-transparent">
+                            <img src="http://localhost:8000/wp-content/uploads/2022/07/clover.png" class="w-[372px] h-auto" />
+                        </div>
+                        <div class="bg-transparent">
+                            <img src="http://localhost:8000/wp-content/uploads/2022/07/removal.ai_tmp-62c93604aa635.png" class="w-[372px] h-auto" />
+                        </div>
+
+                        <div class="col-start-1 col-end-3 bg-transparent mx-auto">
+                            <img src="http://localhost:8000/wp-content/uploads/2022/07/removal.ai_tmp-62c931f780028.png" class="w-[372px]" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+            <div class="relative">
+                <div class="max-w-7xl mx-auto">
+                    <div class="py-20">
+                        <h3 class="text-4xl font-bold mb-10 text-[#B96D3C]">About PT Sinergi Agricom</h3>
+                        <div class="flex justify-between mx-auto items-center space-x-16">
+                            <p>he company started operating in 2022  when it was officially registered under Indonesian Companies Act. The Company is Located and operates in Tangerang, Indonesia with Warehouse Units located at Tangerang and Medan regions.</p>
+                            <p>The company value proposition consists of creative initiatives driven by team of young dynamic experts who belong from different sectors which makes the company more stronger than the existing competitors of market.</p>
+                            <p>The company value proposition consists of creative initiatives driven by team of young dynamic experts who belong from different sectors which makes the company more stronger than the existing competitors of market.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="relative">
+                
+                <div class="max-w-7xl mx-auto">
+                    <div class="py-20">
+                        <div class="grid grid-cols-4">
+                            <div class="flex-col pr-10 pt-20">
+                                <h3 class="text-3xl font-bold mb-10 text-[#B96D3C]">Product Section</h3>
+                                <p class="text-base">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ornare ne.</p>
+                            </div>
+
+                            <div class="col-span-3 bg-[#FCFCFA] p-1">
+                                <div class="grid grid-cols-3">
+                                    <div class="h-80 w-80 p-3">
+                                        <div class="bg-slate-200 w-full h-full rounded-lg border-t-[12px] border-t-orange-200"></div>
+                                    </div>
+
+                                    <div class="h-80 w-80 p-3">
+                                        <div class="bg-slate-200 w-full h-full rounded-lg"></div>
+                                    </div>
+
+                                    <div class="h-80 w-80 p-3">
+                                        <div class="bg-slate-200 w-full h-full rounded-lg"></div>
+                                    </div>
+
+                                    <div class="h-80 w-80 p-3">
+                                        <div class="bg-slate-200 w-full h-full rounded-lg"></div>
+                                    </div>
+
+                                    <div class="h-80 w-80 p-3">
+                                        <div class="bg-slate-200 w-full h-full rounded-lg"></div>
+                                    </div>
+
+                                    <div class="h-80 w-80 p-3">
+                                        <div class="bg-slate-200 w-full h-full rounded-lg"></div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div><!-- Product End -->
+
+            <div class="relative">
+                <div class="max-w-7xl mx-auto">
+                    <div class="bg-orange-300 rounded-lg">    
+                        <div class="p-10 py-14">
+                            <div class="flex justify-between items-center">
+                                <div class="w-4/5">
+                                    <h3 class="text-4xl font-bold mb-5">
+                                        Want to collaborate with us?
+                                    </h3>
+                                    <h5 class="text-xl font-light">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    </h5>
+                                </div>
+
+                                <div class="flex-col space-y-5 w-1/5">
+                                    <button class="bg-white px-7 py-4 text-slate-600 text-lg rounded-lg w-full">Contact Us</button>
+                                    <button class="bg-transparent border-1 border-white px-7 py-4 text-white text-lg rounded-lg w-full">See Our Company</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+
+        
+    </div>
+
+</main>
